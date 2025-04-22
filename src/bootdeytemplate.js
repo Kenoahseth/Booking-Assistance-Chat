b0120e06940b16f824ee39105f923c2d0aba1f65
Kenoahseth <oahhabaradas@gmail.com>
2025-04-22 13:01:44 +0800

    used React template

--- src/bootdeytemplate.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BootdeyTemplate = () => {
-  return (
-    <div dangerouslySetInnerHTML={{ __html: require('./Bootdeytemplate.html') }} />
-  );
-};
-
-export default BootdeyTemplate;
---
+++ temp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BootdeyTemplate from './BootdeyTemplate';
+
+function App() {
+  return (
+    <div className="App">
+      <BootdeyTemplate />
+    </div>
+  );
+}
+
+export default App;
